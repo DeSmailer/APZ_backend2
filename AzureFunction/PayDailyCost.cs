3580b1dc5e449b7f637505806bda344f15bf4f57
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2021-06-01 17:13:11 +0300

    v11

--- AzureFunction/PayDailyCost.cs
+++ AzureFunction/PayDailyCost.cs
@@ -1,6 +1,4 @@
-using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Logging;
 using BusinessLogicLayer.Interfaces;
 using BusinessLogicLayer.Services;
 using DataAccessLayer.Models.Interfaces;
@@ -17,11 +15,10 @@ namespace AzureFunction
         }
 
         [FunctionName("PayDailyCost")]
-        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer)
         {
             IWalletService walletService = new WalletService(repository);
             walletService.PayDailyCost();
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
 }
